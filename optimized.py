0d67f45adb79a001ae03315411c0db36c624c444
Jennifer Cotte <jennifer.malacain@laposte.net>
2022-10-03 12:21:02 +0200

    algo optimisé pour les datas de Sienna

--- optimized.py
+++ optimized.py
@@ -1,10 +1,10 @@
 import csv
 
 COUT_MAX = 500
-DATA = "data/data_brute.csv"
+DATA = "data/dataset1_Python+P7.csv"
 
 with open(DATA) as fichier:
-    reader = csv.DictReader(fichier, delimiter=";")
+    reader = csv.DictReader(fichier, delimiter=",")
     data = []
     for ligne in reader:
         data.append(ligne)
@@ -14,13 +14,20 @@ meilleur_investissement = {"Actions": [], "Coût": 0, "Profit": 0,
 liste_actions = []
 total_cout = 0
 total_profit = 0
-data = sorted(data, key=lambda action: float(action["benefice"]), reverse=True)
 for action in data:
-    if float(action["price"]) + total_cout <= COUT_MAX:
-        liste_actions.append(action["name"])
-        total_cout += float(action["price"])
-        profit = float(action["price"]) * float(action["benefice"]) / 100
-        total_profit += profit
+    if float(action["price"]) < 3 or float(action["profit"]) <= 0:
+        benefice = 0
+    else:
+        benefice = float(action["profit"]) / float(action["price"]) * 100
+    action["benefice"] = benefice
+
+data = sorted(data, key=lambda action: action["benefice"], reverse=True)
+for action in data:
+    if float(action["price"]) >= 3 and float(action["profit"]) > 0:
+        if float(action["price"]) + total_cout <= COUT_MAX:
+            liste_actions.append(action["name"])
+            total_cout += float(action["price"])
+            total_profit += float(action["profit"])
 
 meilleur_investissement["Actions"] = liste_actions
 meilleur_investissement["Coût"] = round(total_cout, 2)
