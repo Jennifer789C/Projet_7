51a9fd0244a2f97b66289c542dea0f499d87c7f3
Jennifer Cotte <jennifer.malacain@laposte.net>
2022-10-06 12:14:39 +0200

    ajout d'un second tri

--- optimized.py
+++ optimized.py
@@ -14,21 +14,42 @@ meilleur_investissement = {"Actions": [], "Coût": 0, "Gain": 0,
 liste_actions = []
 total_cout = 0
 total_gain = 0
-for action in data:
-    if float(action["price"]) <=0:
-        """exclusion des actions ayant des valeurs suspectes (non valables)"""
-        gain = 0
-    else:
-        gain = float(action["price"]) * float(action["profit"]) / 100
-    action["gain"] = gain
 
 data = sorted(data, key=lambda action: float(action["profit"]), reverse=True)
 for action in data:
-    if action["gain"] != 0:
+    if float(action["price"]) > 0:
+        """exclusion des actions ayant des valeurs suspectes (non fiables)"""
         if float(action["price"]) + total_cout <= COUT_MAX:
             liste_actions.append(action["name"])
             total_cout += float(action["price"])
-            total_gain += action["gain"]
+            gain = float(action["price"]) * float(action["profit"]) / 100
+            total_gain += gain
+            index = data.index(action) + 1
+            action1 = data[index]
+            while float(action1["price"]) <= 0:
+                index = index + 1
+                action1 = data[index]
+            if total_cout + float(action1["price"]) >= COUT_MAX:
+                break
+
+cout_restant = COUT_MAX - total_cout
+data_restante = []
+for action in data:
+    if action["name"] in liste_actions:
+        continue
+    if 0 < float(action["price"]) <= cout_restant:
+        """exclusion des actions ayant des valeurs suspectes (non fiables)"""
+        gain = float(action["price"]) * float(action["profit"]) / 100
+        action["gain"] = gain
+        data_restante.append(action)
+
+data_restante = sorted(data_restante, key=lambda action: action["gain"],
+                       reverse=True)
+for action in data_restante:
+    if float(action["price"]) + total_cout <= COUT_MAX:
+        liste_actions.append(action["name"])
+        total_cout += float(action["price"])
+        total_gain += action["gain"]
 
 meilleur_investissement["Actions"] = liste_actions
 meilleur_investissement["Coût"] = round(total_cout, 2)
